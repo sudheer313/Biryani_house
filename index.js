9f3a166f92a50fa35f2ec0e15969b8212746ad00
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-08 20:00:24 +1100

    update menu routes and differitiate seperately

--- index.js
+++ index.js
@@ -3,22 +3,28 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("hello world!");
 });
-//routes for menu
-app.get("/menu", (req, res) => {
-  const menuItems = [
-    {
-      name: "Chicken Biryani",
-      description:
-        "Aromatic basmati rice cooked with tender chicken and spices",
-      price: 10.99,
-    },
-    {
-      name: "Vegetable Biryani",
-      description: "A vegetarian version of our aromatic basmati rice dish",
-      price: 8.99,
-    },
-  ];
-  res.json(menuItems);
+//Define menu items array
+const menuItems = [
+  {
+    name: "Chicken Biryani",
+    description: "Aromatic basmati rice cooked with tender chicken and spices",
+    price: 10.99,
+  },
+  {
+    name: "Vegetable Biryani",
+    description: "A vegetarian version of our aromatic basmati rice dish",
+    price: 8.99,
+  },
+];
+
+//Route for menu Item 1
+app.get("/menu/1", (req, res) => {
+  res.json(menuItems[0]);
+});
+
+//Route for menu Item 2
+app.get("/menu/2", (req, res) => {
+  res.json(menuItems[1]);
 });
 
 //routes for orders
