130d32284c0b378734947000e08d7a8045e0ba03
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-08 19:58:25 +1000

    feat: Integrate Tailwind CSS and create basic restaurant layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi there Welcome to biryani house application</h1>
+         <Header/>
     </div>
   );
 }
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Header() {
+  return (
+    <header className="bg-indigo-600 text-white py-6">
+      <div className="container mx-auto">
+        <h1 className="text-4xl font-bold">Biryani House</h1>
+      </div>
+    </header>
+  );
+}
+export default Header;
