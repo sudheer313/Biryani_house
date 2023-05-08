33068a8c3c897bbc6bfe052bf774669a3a10bc55
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-09 04:59:23 +1000

    feat:update App.tsx with raw data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import MenuItem from './components/MenuItem';
 
 function App() {
+  const sampleItem = {
+    id: 1,
+    name: 'Chicken Biryani',
+    image: 'https://example.com/chicken-biryani.jpg',
+    description: 'Aromatic basmati rice cooked with tender chicken and spices',
+    price: '10.99',
+  };
+
+  const addToCart = (id: number) => {
+    console.log(`Item with ID ${id} added to the cart.`);
+  };
+
   return (
     <div className="App">
-         <Header/>
+      <Header />
+      <MenuItem item={sampleItem} addToCart={addToCart} />
     </div>
   );
 }
