1a20c12c5803666b050ea578702e3742c1d43338
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-17 19:54:01 +1000

    updated biryani image in home page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,13 @@
 // src/pages/Home.tsx
 
 import React from "react";
-
+import biryani from "../images/biryani.jpg";
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome to our Restaurant</h1>
+      <img src={biryani} alt="biryani" className="w-full h-auto object-cover"/>
+
       <p>
         We serve the best food in town. Feel free to browse our menu and place
         your order.
