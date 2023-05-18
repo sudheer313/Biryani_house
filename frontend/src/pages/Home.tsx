b8ceaece463db6f9e0d497ba43556d392f0a3275
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-05-18 10:29:11 +1000

    update home page image height width and intro notes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,12 +4,21 @@ import React from "react";
 import biryani from "../images/biryani.jpg";
 const Home: React.FC = () => {
   return (
-    <div>
-      <img src={biryani} alt="biryani" className="w-full  object-cover  h-80" />
-      <p>
-        We serve the best food in town. Feel free to browse our menu and place
-        your order.
-      </p>
+    <div className="relative bg-black h-96">
+      <img
+        src={biryani}
+        alt="biryani"
+        className="w-full  object-cover  h-screen"
+      />
+      <div className="absolute top-0 left-0 w-full h-full flex flex-col justify-center items-center bg-black bg-opacity-50">
+        <h2 className="text-2xl text-white font-semibold">
+          Welcome to Our Restaurent
+        </h2>
+        <p className="text-white">
+          We serve the best food in town. Feel free to browse our menu and place
+          your order.
+        </p>
+      </div>
     </div>
   );
 };
